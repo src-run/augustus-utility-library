a6d6eff29cc8e10db2f10a7e2214aac48d11d214
Rob Frawley 2nd <rmf@src.run>
2016-04-26 11:47:24 -0400

    added EngineInspect::extensionLoaded()

---
+++ lib/EngineInspect.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * This file is part of the `src-run/wonka-library` project.
+ *
+ * (c) Rob Frawley 2nd <rmf@src.run>
+ * (c) Scribe Inc      <scr@src.run>
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+namespace SR\Utility;
+
+/**
+ * Class EngineInspect
+ */
+class EngineInspect
+{
+    /**
+     * @param string[] $extensions
+     *
+     * @return bool
+     */
+    final public static function extensionLoaded(...$extensions)
+    {
+        if (count($extensions) === 0) {
+            throw new \InvalidArgumentException('No extensions provided for loaded check');
+        }
+
+        $loaded = array_filter($extensions, function ($extension) {
+            return extension_loaded($extension);
+        });
+
+        return $loaded === $extensions;
+    }
+}
+
+/* EOF */
---
+++ tests/EngineInspectTest.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of the `src-run/augustus-utility-library` project.
+ *
+ * (c) Rob Frawley 2nd <rmf@src.run>
+ * (c) Scribe Inc      <scr@src.run>
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+namespace SR\Utility\Tests;
+
+use SR\Utility\EngineInspect;
+
+/**
+ * Class EngineInspectTest
+ */
+class EngineInspectTest extends AbstractTest
+{
+    static public $fixtureData = [
+        'mysqli',
+        'invalid',
+        'mysqli',
+        'invalid'
+    ];
+
+    public function testLoaded()
+    {
+        $assertions = [
+            'EngineInspect::extensionLoaded' => [
+                [true],
+                [false],
+                [true],
+                [false],
+            ]
+        ];
+
+        $this->runThroughAssertions($assertions);
+
+        $this->expectException('\InvalidArgumentException');
+        $this->expectExceptionMessage('No extensions provided for loaded check');
+
+        EngineInspect::extensionLoaded();
+    }
+}
+
+/* EOF */
